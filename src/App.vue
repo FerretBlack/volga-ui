e77d7755b67c395beda4a228978d4f835ce5682d
Max Gorelov <ferretblack1@gmail.com>
2022-11-08 13:21:50 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import VolgaButton from "./components/VolgaButton.vue";
+</script>
+
+<template>
+  <VolgaButton> Обычная кнопка </VolgaButton>
+  <VolgaButton outlined> Кнопка с рамкой </VolgaButton>
+</template>
---
+++ src/components/VolgaButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button :class="[$style.button, { [$style['-outlined']]: outlined }]">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  setup() {},
+};
+</script>
+
+<style lang="scss" module>
+.button {
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  background: $primary;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+
+  &:hover {
+    opacity: 0.9;
+  }
+
+  &.-outlined {
+    border: 2px solid;
+    background: white;
+    border-color: $primary;
+    color: $primary;
+
+    &:hover {
+      background: $primary;
+      color: white;
+      opacity: 1;
+    }
+  }
+}
+</style>
